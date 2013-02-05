32ee99b89e2d7d0feeb5ff9e6637e1009f5c3881
Mo <m.bartl@headstrong.de>
2013-02-06 00:15:59 +0100

    only shred regular files and when path is last hard link

--- unlink-shred.c
+++ unlink-shred.c
@@ -1,9 +1,11 @@
 /*
  * LD_PRELOAD library that runs shred before unlinking files
  * 
+ * extra verbose on purpose for testing. use with caution.
+ *
  * COMPILE:
  *   gcc -Wall -fPIC -DPIC -c unlink-shred.c
- *   ld -shared -o unlink-shred.so unlink-shred.o -ldl
+ *   ld -shared -o unlink-shred.so unlink-shred.o -ldl -lc
  * 
  * USAGE:
  *   LD_PRELOAD=./unlink-shred.so unlink test
@@ -15,33 +17,64 @@
  * PUBLIC DOMAIN
  */
 
+#define DEBUG
+
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <unistd.h>
 #include <dlfcn.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <string.h>
+
+#ifdef DEBUG
+# define DEBUG_PRINTF(x) printf x
+#else
+# define DEBUG_PRINTF(x) do {} while (0)
+#endif
 
 int unlink(const char *path) {
   static int (*libc_unlink)(const char *path) = NULL;
   char *args[3];
   int status;
+  struct stat fstatus;
 
   if (libc_unlink == NULL)
     libc_unlink = dlsym(RTLD_NEXT, "unlink");
 
+  if (lstat(path,&fstatus)==0) {
+    if ((fstatus.st_mode & S_IFMT) != S_IFREG) {
+       // not a regular file, just unlink
+       DEBUG_PRINTF(("%s is not a regular file, not shredding.\n", path));
+       return libc_unlink(path);
+    }
+  } else {
+    // nothing lstat can handle, just unlink
+    DEBUG_PRINTF(("lstat failed for %s: %s\n", path, strerror(errno)));
+    return libc_unlink(path);
+  }
+  
+  if (fstatus.st_nlink>1) {
+    DEBUG_PRINTF(("%s has more than one hard link, not shredding\n", path));
+    return libc_unlink(path);
+  }
+
+  // regular file, last hard link: shred!
   args[0] = "/usr/bin/shred";
   args[1] = path;
   args[2] = NULL;
   switch (fork()) {
-   case -1: printf("fork error\n"); return -1;
+   case -1: DEBUG_PRINTF(("fork error\n")); return -1;
    case 0: execve(args[0], args, NULL); 
    default: if (wait(&status) < 0) return -1;
   }
   if (WIFEXITED(status) && WEXITSTATUS(status) == 0) {
-   printf("shredded %s before unlink\n", path);
+   DEBUG_PRINTF(("yay. shredded %s before unlink\n", path));
    return libc_unlink(path);
   }
-  printf("failed to shred %s before unlink\n", path);
+  DEBUG_PRINTF(("failed to shred %s before unlink\n", path));
   return libc_unlink(path);
 }
 
